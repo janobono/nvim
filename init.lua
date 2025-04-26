991d3316acdd5b683ab8050ad31c3b1007b45704
janobono <janobono@gmail.com>
2025-04-26 18:06:01 +0200

    telescope
    Signed-off-by: janobono <janobono@gmail.com>

--- init.lua
+++ init.lua
@@ -19,6 +19,7 @@ require('lazy').setup {
     require 'plugins.bufferline',
     require 'plugins.lualine',
     require 'plugins.treesitter',
+    require 'plugins.telescope',
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
